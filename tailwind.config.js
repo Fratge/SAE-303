1085048f942efb75ac3f4df9e4c004fdd1d94bd3
Fratge <114362943+Fratge@users.noreply.github.com>
2023-02-15 12:42:59 +0100

    Ajout de la première section + début de le seconde

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./dist/*{.html, js}"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '120': '47rem'
+      },
+      height: {
+        '110': '34rem'
+      },
+      fontSize: {
+        '10xl': '12rem',
+      },
+      fontFamily: {
+        'inter': ['Inter']
+      }
+    },
   },
   plugins: [],
 }
+
