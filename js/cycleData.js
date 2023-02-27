037c7aa9ced5482ad4a4210c44e56ed68753ba83
Fratge <114362943+Fratge@users.noreply.github.com>
2023-02-27 23:01:25 +0100

    +changement arborescence

--- js/cycleData.js
+++ js/cycleData.js
@@ -2,42 +2,42 @@ const images = [
     {
         titre: 'L\'évaporation: L\'eau se transforme en vapeur d\'eau dans l\'atmosphère',
         source: ['Garen', 'https://google.fr'],
-        imageURL: '../src/images/cycle1.png',
+        imageURL: './src/images/cycle1.png',
         gras: ['L\'évaporation', 'transforme', 'atmosphère']
     },
 
     {
         titre: 'La transpiration : Les plantes relâchent de l\'eau dans l\'atmosphère',
         source: ['Frank', 'https://google.fr'],
-        imageURL: '../src/images/cycle2.png',
+        imageURL: './src/images/cycle2.png',
         gras: ['transpiration', 'relâchent', 'l\'eau']
     },
 
     {
         titre: 'La précipitation : L\'eau tombe de l\'atmosphère sous forme de pluie, neige ou grêle',
         source: ['Brut', 'https://google.fr'],
-        imageURL: '../src/images/cycle3.png',
+        imageURL: './src/images/cycle3.png',
         gras: ['précipitation', 'tombe', 'pluie']
     },
 
     {
         titre: 'Le ruissellement : L\'eau s\'écoule en surface en formant des ruisseaux et rivières',
         source: ['UFC', 'https://google.fr'],
-        imageURL: '../src/images/cycle4.png',
+        imageURL: './src/images/cycle4.png',
         gras: ['ruissellement', 's\'écoule', 'formant']
     },
 
     {
         titre: 'L\'infiltration : L\'eau s\'infiltre dans le sol pour remplir les nappes phréatiques',
         source: ['Wikipédia', 'https://google.fr'],
-        imageURL: '../src/images/cycle5.png',
+        imageURL: './src/images/cycle5.png',
         gras: ['L\'infiltration', 's\'infiltre', 'remplir']
     },
 
     {
         titre: 'L\'écoulement : L\'eau peut être pompée ou s\'écouler vers les océans pour recommencer le cycle.',
         source: ['Nasa', 'https://google.fr'],
-        imageURL: '../src/images/cycle6.png',
+        imageURL: './src/images/cycle6.png',
         gras: ['L\'écoulement', 'pompée', 'recommencer']
     }
 ];
